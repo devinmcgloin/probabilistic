25c262f0c16f289f224c4d22fceccfb22a301b3b
Devin McGloin <devin@devinmcgloin.com>
2017-12-27 23:28:02 -0800

    Broke hashing out in to sperate module

--- pkg/bloom/bloom.go
+++ pkg/bloom/bloom.go
@@ -1,48 +1,15 @@
 package bloom
 
 import (
-	"crypto/md5"
-	"crypto/sha1"
-	"crypto/sha256"
-	"crypto/sha512"
-	"encoding/binary"
-	"hash/crc64"
-	"hash/fnv"
 	"math"
+
+	"github.com/devinmcgloin/probabilistic/pkg/hashHelpers"
 )
 
 type Bloom struct {
 	Hashes  uint64
 	Buckets uint64
 	m       map[uint64]bool
-}
-
-// TODO this needs to be extended to support values for k larger than 11. This can be done in multiples of 11 by feeding the output from last round into the next. We can also take every 3rd bit and construct new hashes that way.
-func getHashes(k uint64, data []byte) []uint64 {
-	sums := []uint64{}
-
-	md5Bytes := md5.Sum(data)
-	sums = append(sums, binary.LittleEndian.Uint64(md5Bytes[:]))
-	sums = append(sums, binary.BigEndian.Uint64(md5Bytes[:]))
-	sha1Bytes := sha1.Sum(data)
-	sums = append(sums, binary.LittleEndian.Uint64(sha1Bytes[:]))
-	sums = append(sums, binary.BigEndian.Uint64(sha1Bytes[:]))
-	sha256Bytes := sha256.Sum256(data)
-	sums = append(sums, binary.LittleEndian.Uint64(sha256Bytes[:]))
-	sums = append(sums, binary.BigEndian.Uint64(sha256Bytes[:]))
-	sha512Bytes := sha512.Sum512(data)
-	sums = append(sums, binary.LittleEndian.Uint64(sha512Bytes[:]))
-	sums = append(sums, binary.BigEndian.Uint64(sha512Bytes[:]))
-
-	fnvHash := fnv.New32()
-	fnvBytes := fnvHash.Sum(data)
-	sums = append(sums, binary.LittleEndian.Uint64(fnvBytes[:]))
-	sums = append(sums, binary.BigEndian.Uint64(fnvBytes[:]))
-
-	crc64Table := crc64.MakeTable(0xC96C5795D7870F42)
-	crc64Int := crc64.Checksum(data, crc64Table)
-	sums = append(sums, crc64Int)
-	return sums[:k]
 }
 
 func OptimalBuckets(n uint64, p float64) uint64 {
@@ -77,7 +44,7 @@ func (b Bloom) EstimateSize() float64 {
 }
 
 func (b Bloom) Add(data []byte) {
-	hashes := getHashes(b.Hashes, data)
+	hashes := hashHelpers.GetHashes(b.Hashes, data)
 	for _, h := range hashes {
 		b.m[h%b.Buckets] = true
 	}
@@ -85,7 +52,7 @@ func (b Bloom) Add(data []byte) {
 
 func (b Bloom) Contains(data []byte) bool {
 	found := true
-	hashes := getHashes(b.Hashes, data)
+	hashes := hashHelpers.GetHashes(b.Hashes, data)
 	for _, h := range hashes {
 		if !b.m[h%b.Buckets] {
 			found = false
---
+++ pkg/hashHelpers/hash.go
@@ -0,0 +1,39 @@
+package hashHelpers
+
+import (
+	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
+	"encoding/binary"
+	"hash/crc64"
+	"hash/fnv"
+)
+
+// TODO this needs to be extended to support values for k larger than 11. This can be done in multiples of 11 by feeding the output from last round into the next. We can also take every 3rd bit and construct new hashes that way.
+func GetHashes(k uint64, data []byte) []uint64 {
+	sums := []uint64{}
+
+	md5Bytes := md5.Sum(data)
+	sums = append(sums, binary.LittleEndian.Uint64(md5Bytes[:]))
+	sums = append(sums, binary.BigEndian.Uint64(md5Bytes[:]))
+	sha1Bytes := sha1.Sum(data)
+	sums = append(sums, binary.LittleEndian.Uint64(sha1Bytes[:]))
+	sums = append(sums, binary.BigEndian.Uint64(sha1Bytes[:]))
+	sha256Bytes := sha256.Sum256(data)
+	sums = append(sums, binary.LittleEndian.Uint64(sha256Bytes[:]))
+	sums = append(sums, binary.BigEndian.Uint64(sha256Bytes[:]))
+	sha512Bytes := sha512.Sum512(data)
+	sums = append(sums, binary.LittleEndian.Uint64(sha512Bytes[:]))
+	sums = append(sums, binary.BigEndian.Uint64(sha512Bytes[:]))
+
+	fnvHash := fnv.New32()
+	fnvBytes := fnvHash.Sum(data)
+	sums = append(sums, binary.LittleEndian.Uint64(fnvBytes[:]))
+	sums = append(sums, binary.BigEndian.Uint64(fnvBytes[:]))
+
+	crc64Table := crc64.MakeTable(0xC96C5795D7870F42)
+	crc64Int := crc64.Checksum(data, crc64Table)
+	sums = append(sums, crc64Int)
+	return sums[:k]
+}
