eef5ad8de8a6607c8d17166043547ab5033d8975
Devin McGloin <devin@devinmcgloin.com>
2018-02-01 19:44:53 -0800

    Added documentation for min hash

--- pkg/min-hash/hash.go
+++ pkg/min-hash/hash.go
@@ -8,24 +8,31 @@ import (
 	"github.com/devinmcgloin/probabilistic/pkg/hashHelpers"
 )
 
+// MinHash includes all fields necessary to calculate the min hash between two sets.
 type MinHash struct {
 	k uint64
 }
 
+// New creates a new hash with k set to the number of hashes that
+// ensures an error rate of eps.
 func New(eps float64) MinHash {
 	mh := MinHash{}
 	mh.k = mh.OptimalHashes(eps)
 	return mh
 }
 
+// OptimalHashes calculates the optimal k for a given error rate
 func (mh MinHash) OptimalHashes(eps float64) uint64 {
 	return uint64(1 / (eps * eps))
 }
 
+// ErrorRate is the inverse of OptimalHashes and returns the current expected error rate
 func (mh MinHash) ErrorRate() float64 {
 	return 1 / math.Sqrt(float64(mh.k))
 }
 
+// Estimate takes two sets and returns an estimation of the Jaccard similarity with in
+// an error rate eps of the true Jaccard Similarity.
 func (mh MinHash) Estimate(a []interface{}, b []interface{}) float64 {
 	y := 0.0
 	for i := uint64(0); i < mh.k; i++ {
@@ -36,6 +43,7 @@ func (mh MinHash) Estimate(a []interface{}, b []interface{}) float64 {
 	return y / float64(mh.k)
 }
 
+// hashMin calculates the min hash for a given an FNVHash with bias.
 func hashMin(a []interface{}, bias uint64) uint64 {
 	var min uint64
 	min = math.MaxUint64
@@ -52,6 +60,7 @@ func hashMin(a []interface{}, bias uint64) uint64 {
 	return min
 }
 
+// getBytes returns the []byte array for an interface.
 func getBytes(a interface{}) ([]byte, error) {
 	var buf bytes.Buffer
 	enc := gob.NewEncoder(&buf)
@@ -62,6 +71,8 @@ func getBytes(a interface{}) ([]byte, error) {
 	return buf.Bytes(), nil
 }
 
+// NaiveJaccard calculates the Jaccard similarity through brute force.
+// Used as a comparison in testing Estimate.
 func NaiveJaccard(a []string, b []string) float64 {
 	union := len(a) + len(b)
 	intersection := 0.0
